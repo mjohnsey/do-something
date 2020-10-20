7d58ef58c62ed460a6416863e63a144f3088319e
Michael Johnsey <mjohnsey@gmail.com>
2020-10-19 23:26:49 -0500

    Update members.ts

--- src/commands/members.ts
+++ src/commands/members.ts
@@ -1,10 +1,10 @@
 import {Command} from '@oclif/command'
 import {CongressAPI} from 'propublica-congress-sdk'
-import * as _ from 'lodash'
+// import * as _ from 'lodash'
 
 import {Config} from '../utils'
 
-export default class MembersCommand extends Command {
+export class MembersCommand extends Command {
   static description = 'describe the command here'
 
   static examples = [
@@ -15,16 +15,16 @@ export default class MembersCommand extends Command {
 
   static args = [
     {name: 'chamber', required: true, options: ['house', 'senate']},
-    {name: 'congressNum', required: false, options: _.range(80, 116, 1)}
+    // {name: 'congressNum', required: false, options: _.range(80, 116, 1)},
   ]
 
   async run() {
     const {args} = this.parse(MembersCommand)
     const chamber = args.chamber
-    const congressNum = args.chamber
+    // const congressNum = args.chamber
     const config = Config.loadConfig(this.config.configDir)
     const propublicaApiKey = config.ConfigToml.propublica.api_key
-    const client = new CongressAPI({apiKey: propublicaApiKey});
+    const client = new CongressAPI({apiKey: propublicaApiKey})
     const result = await client.getAllMembers({chamber: chamber})
     console.log(JSON.stringify(result))
   }
